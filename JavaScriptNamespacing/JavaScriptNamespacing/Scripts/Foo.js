2218f0725103a0df521737e8c65ce95961a90f5d
karlinskid <akarlisky@msn.com>
2015-03-08 18:00:50 -0600

    Project Added

---
+++ JavaScriptNamespacing/JavaScriptNamespacing/Scripts/Foo.js
@@ -0,0 +1,30 @@
+﻿/* FOO Namespace */
+;FOO = {
+    name: 'FOO',
+    myFunction: function () {
+        $('#div1').html('This is from namespace FOO');
+    }
+}
+
+
+/* BAR Namespace */
+;BAR = {
+    name: 'BAR',
+    myFunction: function () {
+        $('#div2').html('This is from namespace BAR');
+    }
+}
+
+
+// Extending functionality
+BAR.myFunctionExtend = function () {
+    $('#div3').html('This was an extension of namespace BAR');
+};
+
+// JQUERY Namespace
+$(function () {
+    // Note that the functions have the same names, but different namespaces
+    BAR.myFunction();
+    FOO.myFunction();
+    BAR.myFunctionExtend();
+});
